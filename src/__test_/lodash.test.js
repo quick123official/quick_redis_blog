dfbb43305b7609ccefb37b23e33440d9475d3199
zhoudian <zhoudian@yy.com>
2021-11-27 23:04:50 +0800

    点击 tab 支持命令提示

--- src/__test_/lodash.test.js
+++ src/__test_/lodash.test.js
@@ -1,20 +1,20 @@
+var lodash = require("lodash");
 test("lodash", () => {
-    let dbIndexMap = new Map();
-    dbIndexMap.set(1, {
-        key: "" + 1,
-        dbIndex: 1,
-        title: 1,
-        total: 0,
-    });
-    dbIndexMap.set(20, {
-        key: "" + 1,
-        dbIndex: 20,
-        title: 1,
-        total: 0,
-    });
-    for ([key, value] of dbIndexMap) {
-        if (value.dbIndex >= 16) {
-            console.info("", key, value);
-        }
-    }
+    const arr = [{
+        label: 'All',
+        value: 'All'
+    }, {
+        label: 'All',
+        value: 'All'
+    }, {
+        label: 'Alex',
+        value: 'Ninja'
+    }, {
+        label: 'Bill',
+        value: 'Op'
+    }, {
+        label: 'Cill',
+        value: 'iopop'
+    }];
+    lodash.uniqWith();
 });
